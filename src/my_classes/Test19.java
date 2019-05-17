81664485fa48f360e45333ced88f1ec454afb399
fortune404 <rk815.rahi@gmail.com>
2019-05-17 16:48:19 -0700

    added new class

---
+++ src/my_classes/Test19.java
@@ -0,0 +1,21 @@
+package my_classes;
+
+public class Test19
+{
+    public static void main(String[] args) {
+        String coin = "quarter";
+
+        if (coin.equalsIgnoreCase("quarter")){
+            System.out.println("this is a quarter");
+        }else if (coin.equalsIgnoreCase("dime")){
+            System.out.println("this is a dime");
+        }else if (coin.equalsIgnoreCase("nickle")){
+            System.out.println("this is a nickle");
+        }else if (coin.equalsIgnoreCase("penny")){
+            System.out.println("this is a penny");
+        }else {
+            System.out.println("this is a wrong input");
+        }
+    }
+
+}
