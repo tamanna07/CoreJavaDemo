c0488c8755b05d46ce287c842d23198fd55fd382
fortune404 <rk815.rahi@gmail.com>
2019-05-19 03:08:40 -0700

    added new class of loops

---
+++ src/my_classes/Test21.java
@@ -0,0 +1,21 @@
+package my_classes;
+
+public class Test21
+{
+    public static void main(String[] args)
+    {
+        int x = 10;
+        do {
+            System.out.println(x+=2);
+        }while (x<1);
+
+
+
+       //for (int v = 0; v<=10;v+=2){
+          // System.out.println(v);
+
+       //}
+
+    }
+}
+
