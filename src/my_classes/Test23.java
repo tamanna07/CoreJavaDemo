32e233ac32f6eda05300c0806dde430663b4f251
fortune404 <rk815.rahi@gmail.com>
2019-05-27 20:20:35 -0700

    added new classes

---
+++ src/my_classes/Test23.java
@@ -0,0 +1,48 @@
+package my_classes;
+
+public class Test23
+{
+    /*
+
+    1. David
+    2. Ahmed
+    3. John
+    4. Abdul
+    5. Nafee
+
+    ArrayList
+    LinkedList
+
+     */
+
+    public static void main(String[] args) {
+
+        String[] names = new String[5];
+
+        names[0] = "David";
+        names[1] = "Ahmed";
+        names[2] = "John";
+        names[3] = "Abdul";
+        names[4] = "Nafee";
+
+        //System.out.println(names.length);
+
+       // for (int i = 0; i < names.length; i++){
+
+           // System.out.println(names[i]);
+       // }
+
+        int i = 0;
+        while (i < names.length){
+
+            System.out.println(names[i]);
+            i++;
+
+        }
+
+
+
+
+
+    }
+}
