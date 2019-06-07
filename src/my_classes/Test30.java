93fc3ae9873ce52191f02d038aead30117b0b0bb
fortune404 <rk815.rahi@gmail.com>
2019-06-06 18:30:24 -0700

    new classes

---
+++ src/my_classes/Test30.java
@@ -0,0 +1,54 @@
+package my_classes;
+
+public class Test30
+{
+    protected void demo1()
+    {
+        String[] list = new String[3];
+
+        list[0] = "Post malone";
+        list[1] = "Travis Scott";
+        list[2] = "DJ khaled";
+
+        System.out.println(list[0]);
+        System.out.println(list[1]);
+        System.out.println(list[2]);
+
+    }
+
+    protected void demo2()
+    {
+        String[] list = new String[3];
+
+        list[0] = "Post malone";
+        list[1] = "Travis Scott";
+        list[2] = "DJ khaled";
+
+        for (int x = 0; x < 3; x++)
+        {
+            System.out.println(list[x]);
+        }
+
+    }
+
+    //********************************************************
+    //********************************************************
+    //********************************************************
+    //********************************************************
+    //********************************************************
+
+    //diff class
+
+    public static void main(String[] args)
+    {
+        Test30 execute1 = new Test30();
+        execute1.demo1();
+
+        System.out.println("***********");
+
+        execute1.demo2();
+    }
+
+
+
+}
