b835700def1d52fba120ac78f938687b6e67bc67
fortune404 <rk815.rahi@gmail.com>
2019-08-25 22:02:42 -0700

    more practice

---
+++ src/tutoringTrycatch/ExceptionHandlingUsingThrows.java
@@ -0,0 +1,18 @@
+package tutoringTrycatch;
+
+public class ExceptionHandlingUsingThrows
+{
+    public static void main(String[] args)throws ArithmeticException, ArrayIndexOutOfBoundsException
+    {
+        String[] locations = new String[3];
+
+        locations[0] = "NY";
+        locations[1] = "NJ";
+        locations[2] = "PA";
+
+        System.out.println(20/0);
+        //Thread.sleep(5000);                //ex: compile time error.
+        // Thread.sleep(); to pause the program for an amount of time chosen
+        System.out.println("location: "+locations[5]);
+    }
+}
---
+++ src/tutoringTrycatch/UseTryCatch.java
@@ -0,0 +1,62 @@
+package tutoringTrycatch;
+
+import javax.crypto.spec.PSource;
+
+public class UseTryCatch
+{
+    //Java has categorised exception in Checked Exception and Unchecked
+    //RunTimeException is an unchecked exception.
+    public static void main(String[] args)
+    {
+        try {
+                String[] locations = new String[3];
+                Thread.sleep(5000);
+
+                locations[0] = "NY";
+                locations[1] = "NJ";
+                locations[2] = "PA";
+
+            System.out.println(20/2);
+
+
+
+
+
+                System.out.println("location: "+locations[3]);
+        }catch (Exception ex)   //catch (ArithmeticException | ArrayIndexOutOfBoundsException e) example of multiple exceptions in one catch
+        {
+            System.out.println("Sorry! couldn't find location");
+        }//catch (ArithmeticException ex)                           can also be used for multiple custom exceptions
+        //{
+           // System.out.println("Sorry you can't divide 20 by 0");
+        //}
+
+
+
+
+        try {
+            String[] locations = new String[3];
+
+            locations[0] = "NY";
+            locations[1] = "NJ";
+            locations[2] = "PA";
+
+            System.out.println(20/0);
+
+
+
+
+
+            System.out.println("location: "+locations[3]);
+    }catch (ArithmeticException ex)
+        {
+            System.out.println("Unknown location");
+        }finally {
+            System.out.println("you made it!");
+        }
+
+        System.out.println("yo wasssup");
+
+    }
+
+}
